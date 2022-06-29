ecdea49afcdfb6ddfec2f39603b33bdff5a75f82
HOSENUR <nur08439@gmail.com>
2022-06-29 23:11:00 +0530

    Backend CleanUp

--- server/main.py
+++ server/main.py
@@ -1,3 +1,5 @@
+from fastapi import FastAPI
+from email import message
 from typing import Union
 import random
 import json
@@ -5,15 +7,61 @@ import pickle
 import numpy as np
 from nltk.stem import WordNetLemmatizer
 import nltk
-from tensorflow.keraa.models import load_model
+from fastapi import Request
+from tensorflow.keras.models import load_model
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+model = load_model('model.h5')
+from EMOJIS import DATA
 
-from fastapi import FastAPI
+def clean(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag(sentence):
+    sentence_words = clean(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict(sentence):
+    bow = bag(sentence)
+    res= model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def getResponse(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag'] == tag):
+            result = random.choice(i['responses'])
+            break
+    return result        
 
 app = FastAPI()
-
 
 @app.get("/ping")
 def read_root():
     return {"res": "pong"}
+
+@app.post("/popsicle")
+async def translate(info : Request):
+    req = await info.json()
+    ints = predict(req['text'])
+    res = DATA[getResponse(ints, intents)]
+    return {
+        "status" : "SUCCESS",
+        "res" :res,
+    }
