43b230258ade107e4e9d240449c29abbd5b7d8d5
HOSENUR <nur08439@gmail.com>
2022-06-29 21:38:13 +0530

    Completed Neural Network

--- server/main.py
+++ server/main.py
@@ -1,4 +1,13 @@
 from typing import Union
+import random
+import json
+import pickle
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+import nltk
+from tensorflow.keraa.models import load_model
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
 
 from fastapi import FastAPI
 
--- server/training.py
+++ server/training.py
@@ -1,11 +1,17 @@
+from copyreg import pickle
 import random
 import json
 import numpy as np
 from nltk.stem import WordNetLemmatizer
 import nltk
+import pickle
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation
 from tensorflow.keras.optimizers import SGD
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('omw-1.4')
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
 words = []
@@ -15,9 +21,45 @@ documents = []
 for intent in intents["intents"]:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
-        words.append(word_list)
+        words.extend(word_list)
         documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-print(documents)
+words = [lemmatizer.lemmatize(word) for word in words]
+words = sorted(set(words))
+classes = sorted(set(classes))
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+training = []
+output_empty = [0] * len(classes)
+for document in documents:
+    bag= []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        if word in word_patterns:
+            bag.append(1)
+        else:
+            bag.append(0)
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+random.shuffle(training)
+training = np.array(training)
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('model.h5')
+print("Model trained!")
+
+
+
